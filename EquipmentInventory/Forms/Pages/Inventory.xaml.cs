da3a456847c3ba46567bae8ad58fa8a3f262f77b
Switrue <148533468+Switrue@users.noreply.github.com>
2025-03-02 14:17:06 +0700

    add drawer for Inventory table

--- EquipmentInventory/Forms/Pages/Inventory.xaml.cs
+++ EquipmentInventory/Forms/Pages/Inventory.xaml.cs
@@ -3,6 +3,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Input;
+using MaterialDesignThemes.Wpf;
 
 namespace EquipmentInventory.Forms.Pages
 {
@@ -17,15 +18,27 @@ namespace EquipmentInventory.Forms.Pages
             InitializeComponent();
         }
 
-        private async void toggleGridBtn_Click(object sender, RoutedEventArgs e)
-        {
-            AnimationHelper.ToggleSlideAnimation(filterAreaGrid);
-            await AnimationHelper.AnimatedBlock((ToggleButton)sender);
-        }
-
         private void Page_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            Keyboard.ClearFocus();
+            toggleGridBtn.Focus();
+        }
+
+        private void toggleGridBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var toggle = sender as ToggleButton;
+
+            if (toggle != null)
+            {
+                if (toggle.IsChecked == true)
+                {
+                    DrawerHost.OpenDrawerCommand.Execute(null, null);
+                }
+                else
+                {
+                    DrawerHost.CloseDrawerCommand.Execute(null, null);
+                }
+
+            }
         }
     }
 }
